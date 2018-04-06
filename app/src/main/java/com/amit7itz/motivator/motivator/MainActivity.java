ad41fc1ce9b4b5ae069f54578de75a4ad92fd7db
amit7 <amit7itz@gmail.com>
2018-04-06 15:08:34 +0300

    removed textbox and send button

--- app/src/main/java/com/amit7itz/motivator/motivator/MainActivity.java
+++ app/src/main/java/com/amit7itz/motivator/motivator/MainActivity.java
@@ -1,50 +1,24 @@
 package com.amit7itz.motivator.motivator;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.amit7itz.motivator.MESSAGE";
-    private FileManager file_manager;
     private FileManager money_manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.file_manager = new FileManager("file.txt", this.getApplicationContext());
         this.money_manager = new FileManager("money.txt", this.getApplicationContext());
-        String saved_message = this.file_manager.read();
         String saved_money = this.money_manager.read();
-        if (saved_message != null) {
-            EditText editText = findViewById(R.id.editText);
-            editText.setText(saved_message);
-        }
         if (saved_money != null) {
             TextView moneyText = findViewById(R.id.currentMoney);
             moneyText.setText(saved_money);
         }
-    }
-
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        this.file_manager.write(message);
-        intent.putExtra(EXTRA_MESSAGE, message);
-
-        startActivity(intent);
-    }
-
-    public void clearMessage(View view) {
-        EditText editText = findViewById(R.id.editText);
-        editText.setText("");
     }
 
     public void addMoney(View view) {
