8109910d4b55a09d7ba688f66d89fa8033ffe40d
Amit Itzkovitch <amit7itz@gmail.com>
2018-04-07 14:48:07 +0300

    first version with multiple buttons

--- app/src/main/java/com/amit7itz/motivator/motivator/MainActivity.java
+++ app/src/main/java/com/amit7itz/motivator/motivator/MainActivity.java
@@ -1,9 +1,9 @@
 package com.amit7itz.motivator.motivator;
 
-import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,37 +12,63 @@ import com.amit7itz.motivator.motivator.db.ActivityType;
 import com.amit7itz.motivator.motivator.db.AppDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private AppDatabase db;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "motivator").allowMainThreadQueries().build();
         this.updateTotalReward();
+        this.fillActivitiesTypesViewer();
     }
 
     private void updateTotalReward() {
         TextView moneyText = findViewById(R.id.currentMoney);
-        moneyText.setText(String.format("%s", this.db.activityDao().getTotalReward()));
+        moneyText.setText(String.format("%s", this.getDb().activityDao().getTotalReward()));
+    }
+
+    private void fillActivitiesTypesViewer() {
+        this.mRecyclerView = findViewById(R.id.activity_types_viewer);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        this.mRecyclerView.setHasFixedSize(false);
+
+        // use a linear layout manager
+        this.mLayoutManager = new LinearLayoutManager(this);
+        this.mRecyclerView.setLayoutManager(this.mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] sa = new String[3];
+        sa[0] = "Run";
+        sa[1] = "Walk";
+        sa[2] = "Swim";
+        this.mAdapter = new ActivityTypesAdapter(sa);
+        this.mRecyclerView.setAdapter(this.mAdapter);
+    }
+
+    private AppDatabase getDb() {
+        return AppDatabase.getAppDatabase(this.getApplicationContext());
     }
 
     public void addMoney(View view) {
         long type_id;
-        if (this.db.activityTypeDao().count() == 0) {
+        if (this.getDb().activityTypeDao().count() == 0) {
             ActivityType some_type = new ActivityType();
             some_type.setName("Running");
             some_type.setDescription("bla bla bla");
             some_type.setReward(5);
-            type_id = this.db.activityTypeDao().insert(some_type);
+            type_id = this.getDb().activityTypeDao().insert(some_type);
         }
         else {
-            type_id = this.db.activityTypeDao().first().getId();
+            type_id = this.getDb().activityTypeDao().first().getId();
         }
         Activity act = new Activity();
         act.setActivityTypeId(type_id);
         act.setTimestamp(System.currentTimeMillis() / 1000);
-        this.db.activityDao().insertAll(act);
+        this.getDb().activityDao().insertAll(act);
         this.updateTotalReward();
     }
 }
